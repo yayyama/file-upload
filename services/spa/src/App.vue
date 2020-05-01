f24ac9a1de94b7ddcf3e53a2ac0865b2f144081e
Yoichi Yamaguchi <yoichi.yamaguchi@codist.co.jp>
2020-05-02 01:27:10 +0900

    WIP

--- services/spa/src/App.vue
+++ services/spa/src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FileUpload></FileUpload>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FileUpload from './components/FileUpload'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FileUpload
   }
 }
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ services/spa/src/components/FileUpload.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container"
+       :class="containerClass"
+       @dragenter="dragenter"
+       @dragleave.prevent="dragleave"
+       @dragover="dragover"
+       @drop.prevent="drop"
+  >
+    <div class="uploader"
+    >
+      file upload
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    containerClass: {
+      'on-dragenter': false
+    }
+  }),
+  methods: {
+    dragenter: function (e) {
+      if (e.target === e.currentTarget) {
+        this.containerClass['on-dragenter'] = true
+        console.log('** dragenter ' + new Date().getTime())
+        console.log(e.target)
+      }
+    },
+    dragleave: function (e) {
+      if (e.target === e.currentTarget) {
+        this.containerClass['on-dragenter'] = false
+        console.log('-- dragleave ' + new Date().getTime())
+        console.log(e.target)
+      }
+    },
+    dragover: function () {
+      // console.log('$$ dragover ' + new Date())
+    },
+    drop: function () {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+.uploader {
+  width: 200px;
+  height: 300px;
+  margin: auto;
+  background-color: yellow;
+}
+.on-dragenter {
+  background-color: pink;
+}
+</style>
